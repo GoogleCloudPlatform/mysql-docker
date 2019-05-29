33edb73365b4292cca18a73e2455fb08ea98a48d
ovk6 <kalinichenka@google.com>
2019-05-29 12:40:09 +0200

    Update versions - 2019-05-08, fix _get_config function and added debug option in 8/debian9/8.0/docker-entrypoint.sh (#51)
    * Update versions - 2019-05-08
    
    * Changes in 8/debian9/8.0/docker-entrypoint.sh
    
    Fixed: _get_config fuction for take actual socket path;
    Added: DEBUG_DOCKER_ENTERYPOINT option for docker-entrypoint.sh;
    
    * Fix format in 8/debian9/8.0/docker-entrypoint.sh
    
    * Add comment and fixes for DEBUG_DOCKER_ENTERYPOINT option
    
    * Added: Waring when DEBUG_DOCKER_ENTERYPOINT is enabled
    
    * Return --log-bin-index option in _get_config() function
    
    * Update 8/debian9/8.0/docker-entrypoint.sh
    
    Fix spelling in comments
    
    Co-Authored-By: wgrzelak <wgrzelak@users.noreply.github.com>

--- 8/debian9/8.0/docker-entrypoint.sh
+++ 8/debian9/8.0/docker-entrypoint.sh
@@ -16,6 +16,13 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+# Enable bash debug if DEBUG_DOCKER_ENTERYPOINT exists
+if [[ ! -z "${DEBUG_DOCKER_ENTERYPOINT}" ]]; then
+	echo "!!! WARNING: DEBUG_DOCKER_ENTERYPOINT is enabled!"
+	echo "!!! WARNING: Use only for debugging. Do not use in production!"
+	set -x
+fi
+
 set -eo pipefail
 shopt -s nullglob
 
@@ -76,7 +83,7 @@ _check_config() {
 # latter only show values present in config files, and not server defaults
 _get_config() {
 	local conf="$1"; shift
-	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | awk '$1 == "'"$conf"'" && $3=="" { print $2 }'
+	"$@" --verbose --help --log-bin-index="$(mktemp -u)" 2>/dev/null | grep "^$conf" | awk '$1 == "'"$conf"'" { print $2; exit }'
 }
 
 # allow the container to be started with `--user`
